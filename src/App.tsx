fb18fc1c42493d64c8b5687c875ae9f5b0627424
Joe Podwys <joseph@podwys.com>
2024-07-01 11:40:15 -0600

    Add blur demo changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 import './App.css';
+import { GaussianBlurBackgroundProcessor, Pipeline } from '@twilio/video-processors';
+
+const constraints: MediaStreamConstraints = {
+  video: {
+    width: 640,
+    height: 480,
+    frameRate: 24,
+  },
+};
+
+const blur = new GaussianBlurBackgroundProcessor({
+  assetsPath: 'https://local.evisit.com/resources/twilio/video-processors/1.0.0',
+  pipeline: Pipeline.WebGL2,
+  debounce: false,
+});
 
 function App() {
+  const [stream, setStream] = useState<MediaStream | undefined>();
+  const [modelIsLoaded, setModelIsLoaded] = useState<boolean>(false);
+  const [videoIsPlaying, setVideoIsPlaying] = useState<boolean>(false);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  const doBlur = (video: HTMLVideoElement, canvas: HTMLCanvasElement) => {
+    blur.processFrame(video, canvas);
+    videoRef.current?.requestVideoFrameCallback(() => doBlur(video, canvas));
+  }
+
+  const setBlurAmount = (event: ChangeEvent<HTMLInputElement>) => {
+    blur.blurFilterRadius = Number(event.target.value);
+  };
+
+  useEffect(() => {
+    const video = videoRef.current;
+    if (video) {
+      video.addEventListener('play', () => {
+        setVideoIsPlaying(true);
+      });
+    }
+  }, [videoRef, setVideoIsPlaying]);
+
+  useEffect(() => {
+    blur.loadModel().then(() => {
+      setModelIsLoaded(true);
+    });
+  }, [setModelIsLoaded]);
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia(constraints).then(setStream);
+  }, [setStream]);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    if (stream && video) {
+      video.srcObject = stream;
+    }
+  }, [stream, videoRef]);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    const canvas = canvasRef.current;
+    if (modelIsLoaded && stream && video && canvas && videoIsPlaying) {
+      doBlur(video, canvas);
+    }
+  }, [modelIsLoaded, stream, videoRef, canvasRef, videoIsPlaying]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input type="range" min="5" max="20" onInput={setBlurAmount} />
+      </div>
+      <video ref={videoRef} autoPlay></video>
+      <canvas ref={canvasRef} width="640" height="480"></canvas>
     </div>
   );
 }
